ae173c2ecc714a59edf815993eb1ca042b294b34
Hello <bedrock0055@gmail.com>
2023-10-30 19:07:18 +0200

    Initial commit

---
+++ Dz_2/Sobaka.py
@@ -0,0 +1,32 @@
+import random
+class Sobaka:
+    def __init__(self, name="Sharik", poroda=None):
+        self.name=name
+        self.poroda=poroda
+        self.health=100
+        self.graz=0
+        self.money=100
+        self.happy=100
+        self.sitost=100
+        self.live=True
+    def poroda(self):
+        poroda_cube=(random.randint(1,3))
+        if poroda_cube==1:
+            self.poroda="Chihuahua"
+            print("Ваша порода собаки: Чихуахуа")
+        if poroda_cube==2:
+            self.poroda="Buldog"
+            print("Ваша порода собаки: Бульдог")
+        if poroda_cube==3:
+            self.poroda="Korgi"
+            print("Ваша порода собаки: Корги")
+    def sleep(self):
+        print("Собакин поспал")
+        self.happy+=10
+        self.sitost-=30
+    def eat(self):
+        print("Собакин покушал")
+        self.sitost+=30
+    def walk(self):
+        walk_cube=(random.randint(1,4))
+        
